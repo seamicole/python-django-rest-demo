e38f07f2fcee02e7948c5cfa0cbad936df456623
khunspoonzi <khunspoonzi@gmail.com>
2021-09-12 21:41:58 +0700

    Add user model test

---
+++ user/tests/test_models.py
@@ -0,0 +1,54 @@
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ DJANGO IMPORTS
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+from django.test import TestCase
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ PROJECT IMPORTS
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+from user.models import User
+
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ USER MODEL TEST CASE
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+
+class UserModelTestCase(TestCase):
+    """ User Model Test Case """
+
+    # ┌─────────────────────────────────────────────────────────────────────────────────
+    # │ TEST EMAIL IS SLUGIFIED
+    # └─────────────────────────────────────────────────────────────────────────────────
+
+    def test_email_is_slugified(self):
+        """ Ensures that a user's email address is always slugified """
+
+        # Define email address
+        email = "Jérémy.Dupont@GMAIL.com"
+
+        # Define slugified email
+        email_slugified = "jeremy.dupont@gmail.com"
+
+        # Create user
+        user = User.objects.create_user(email=email, password="password")
+
+        # Assert that created user's email is slugified
+        self.assertEqual(user.email, email_slugified)
+
+        # Reset email to initial unslugified value
+        user.email = email
+
+        # Assert that the instance now has an unslugified email
+        self.assertEqual(user.email, email)
+
+        # Save user to database
+        user.save()
+
+        # Assert that the user instance's email was slugified again on save
+        self.assertEqual(user.email, email_slugified)
+
+        # Assert that this is true on the database side as well
+        self.assertEqual(User.objects.filter(email=email_slugified).count(), 1)
