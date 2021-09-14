9d08a83b6e8ccf00dcf88dd3ed9354dfa8e3a06b
khunspoonzi <khunspoonzi@gmail.com>
2021-09-14 15:31:11 +0700

    Implement country ISO lookups

--- location/views.py
+++ location/views.py
@@ -1,9 +1,16 @@
 # ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ DJANGO IMPORTS
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+from django.db.models import Q
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
 # │ DJANGO REST FRAMEWORK IMPORTS
 # └─────────────────────────────────────────────────────────────────────────────────────
 
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 # ┌─────────────────────────────────────────────────────────────────────────────────────
 # │ PROJECT IMPORTS
@@ -73,6 +80,34 @@ class CountryViewSet(DynamicReadOnlyModelViewSet):
 
     # Define queryset
     queryset = Country.objects.all().prefetch_related("cities").order_by("name")
+
+    # ┌─────────────────────────────────────────────────────────────────────────────────
+    # │ RETRIEVE
+    # └─────────────────────────────────────────────────────────────────────────────────
+
+    def retrieve(self, request, pk=None):
+        """ Custom Retrieve Method """
+
+        # Check if pk is alpha string
+        if pk and pk.isalpha():
+
+            # Uppercase pk value
+            pk = pk.upper()
+
+            # Get country whose ISO code matches pk
+            country = self.get_queryset().filter(Q(iso2=pk) | Q(iso3=pk)).first()
+
+            # Check if country exists
+            if country:
+
+                # Get country serializer
+                serializer = self.get_serializer(country)
+
+                # Return serialized data
+                return Response(serializer.data)
+
+        # Return parent method
+        return super().retrieve(request, pk=pk)
 
 
 # ┌─────────────────────────────────────────────────────────────────────────────────────
